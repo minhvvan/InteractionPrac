853c6fd3190e29ca30da7c6d9be7c2f728da510d
minhvvan <eminhwan12@gmail.com>
2023-04-17 17:09:57 +0900

    Interact Car
    Enter & Out Car (with.BP)

--- Source/InteractionPrac/InteractionPracCharacter.cpp
+++ Source/InteractionPrac/InteractionPracCharacter.cpp
@@ -50,8 +50,6 @@ AInteractionPracCharacter::AInteractionPracCharacter()
 
 
 	IsAttach = false;
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
-	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
 }
 
 void AInteractionPracCharacter::BeginPlay()
