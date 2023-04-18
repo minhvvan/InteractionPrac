535ba01cc01e9e2d52437719cc648faf9ebbacf2
minhvvan <eminhwan12@gmail.com>
2023-04-18 15:25:57 +0900

    Vehicle Update
    Controller Update

--- Source/InteractionPrac/InteractionPracGameMode.cpp
+++ Source/InteractionPrac/InteractionPracGameMode.cpp
@@ -6,7 +6,6 @@
 
 AInteractionPracGameMode::AInteractionPracGameMode()
 {
-	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
